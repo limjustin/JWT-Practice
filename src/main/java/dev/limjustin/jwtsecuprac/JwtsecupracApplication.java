fc488aeff9fe9ec3f4bd6c0d7d22ba8eecb46e1c
Jaeyoung Lim <limjyjustin@gmail.com>
2024-01-12 16:45:31 +0900

    Initial Commit

---
+++ src/main/java/dev/limjustin/jwtsecuprac/JwtsecupracApplication.java
@@ -0,0 +1,13 @@
+package dev.limjustin.jwtsecuprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtsecupracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtsecupracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/limjustin/jwtsecuprac/JwtsecupracApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limjustin.jwtsecuprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtsecupracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
